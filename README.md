be2c0475d172a535ff8fbfd8d87ceed250d705b2
vitoi <vito-lee@outlook.com>
2023-09-15 18:56:12 +0800

    modify vote app

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # dj
 learning to use Django
 
+0915: https://docs.djangoproject.com/zh-hans/4.2/intro/tutorial07/#customize-the-admin-look-and-feel
+
 0912: https://docs.djangoproject.com/zh-hans/4.2/intro/tutorial05/
 
 0911: https://docs.djangoproject.com/en/4.2/intro/tutorial04/
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+from django.contrib import admin
 
 # Create your models here.
 
@@ -11,6 +12,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    
+    @admin.display(
+    boolean=True,
+    ordering="pub_date",
+    description="Published recently?",
+    )
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
